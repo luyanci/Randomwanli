c525fed0c28d1b2886bd3bd5dc6836373ade1357
luyanci <2058282593@qq.com>
2024-08-06 15:32:13 +0800

    update

--- main.py
+++ main.py
@@ -30,23 +30,16 @@ async def get_random():
     endnum=check_type(types)
     #print(endnum)
     num=random.randrange(1,endnum,1)
-    url=f"{hoster}/getwanli-{str(types)}/{num}"
+    url=f"{hoster}/getwanli/{num}?q={str(types)}"
     return {"type":types,"url": url}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/getwanli-png/{filename}")
-async def get_wanli_png(filename:str):
-    filepath= f"res/png/{filename}.png"
+@app.get("/getwanli/{filename}")
+async def get_wanli(filename:str,q: Optional[str]=None):
+    filepath= f"res/{q}/{filename}.{q}"
     if not os.path.exists(filepath):
         return {"code":404,"message":"Not Found!"}
-    return FileResponse(filepath,media_type="image/png")
-
-@app.get("/getwanli-gif/{filename}")
-async def get_wanli_gif(filename:str):
-    filepath= f"res/gif/{filename}.gif"
-    if not os.path.exists(filepath):
-        return {"code":404,"message":"Not Found!"}
-    return FileResponse(filepath,media_type="image/gif")
+    return FileResponse(filepath,media_type=f"image/{q}")
