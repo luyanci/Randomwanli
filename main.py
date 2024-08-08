ac6f2a07cdbc2b79ebc9e3c1675915458d7a72f5
luyanci <2058282593@qq.com>
2024-08-08 19:41:37 +0800

    Update

--- main.py
+++ main.py
@@ -4,32 +4,30 @@ import random
 from dotenv import load_dotenv
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from fastapi.responses import FileResponse,RedirectResponse
 
-
 app = FastAPI()
+use_custom=True
 hoster = "https://randomwanli.zeabur.app"
 load_dotenv("./.env")
-custom=str(os.environ["HOSTER"])
-if len(str(custom)) >= 7:
-    use_custom=True
-    print("Custom enabled!")
+try:
+    custom=str(os.environ["HOSTER"])
+except:
+    use_custom=False
+finally:
+    if use_custom is True and len(str(custom)) >= 7:
+        hoster=custom
+        print("Custom enabled!")
 
 def random_type():
     types=random.randint(1,3)
-    if types == 1:
-        return 'png'
-    if types == 2:
-        return 'gif'
-    if types == 3:
-        return 'jpg'
+    return ['png', 'gif', 'jpg'][types - 1]
 
 def get_end(type:str):
     if type == 'jpg':
         return 'jpeg'
-    else:
-        return type
+    return type
 
 def check_type(type:str):
     num_group=json.load(open("res/file.json",mode="r",errors="ignore"))
@@ -42,10 +40,7 @@ async def get_random(returns:Optional[str]=None,type:Optional[str]=None):
         types=random_type()
     endnum=check_type(types)
     num=random.randint(1,endnum)
-    if use_custom is True:
-        url=f"{custom}/getwanli/{num}?type={str(types)}"
-    else:
-        url=f"{hoster}/getwanli/{num}?type={str(types)}"
+    url=f"{hoster}/getwanli/{num}?type={str(types)}"
     if returns == '307':
         return RedirectResponse(url=url)
     else:
@@ -54,11 +49,21 @@ async def get_random(returns:Optional[str]=None,type:Optional[str]=None):
 @app.get("/favicon.ico")
 def get_favicon():
     return FileResponse("res/favicon.ico",media_type="image/icon")
-    
+
 @app.get("/getwanli/{filename}")
 async def get_wanli(filename:str,type:Optional[str]=None):
+    if type is None or type not in ['jpg','png','gif']:
+        return FileResponse("res/404.png",media_type="image/png")
     typeend=get_end(type)
     filepath= f"res/{type}/{filename}.{type}"
-    if not os.path.exists(filepath):
-        return FileResponse("res/404.png",media_type="image/png")
-    return FileResponse(filepath,media_type=f"image/{typeend}")
+    try:
+        if not os.path.exists(filepath):
+            return FileResponse("res/404.png",media_type="image/png")
+        return FileResponse(filepath,media_type=f"image/{typeend}")
+    except:
+        raise HTTPException(status_code=403, detail="Not Allowed")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
